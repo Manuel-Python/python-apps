b9c82f69741d41b372897fac23a3b1f944170369
Manuel Carvalho <burrowsrd@gmail.com>
2020-12-17 13:11:13 +1100

    Added movies100 cleanup(1)

--- movies100.py
+++ movies100.py
@@ -7,7 +7,37 @@ movies_web_page = response.text
 soup = BeautifulSoup(movies_web_page, "html.parser")
 movies = soup.find_all(name="h3", class_="title")
 
+movies = movies[::-1]
 
-for movie in movies:
     #print(story.getText())
-    print(movie.getText())
+
+movieDB = {}
+num = 0
+count = 1
+name1 = ""
+for movie in movies:
+
+    line = movie.getText()
+    #name = line.split(" ")
+    if ") " in line:
+        name = line.split(" ")
+        isFirst = True
+        for n in name:
+            if isFirst:
+                isFirst = False
+
+            else:
+                name1 += n
+                name1 += " "
+        name1 += "\n"
+        print(name1)
+    else:
+        print(line)
+    count +=1
+
+
+
+# with open("movies.txt", mode="w") as file:
+#     for movie in movies:
+#         print(movie.getText())
+#         file.write(f"{movie.getText()}\n")
