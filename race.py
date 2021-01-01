89b6ae0b8d090ea3651668f66a3f1b0e4c0ddc3d
Manuel Carvalho <burrowsrd@gmail.com>
2021-01-01 20:03:49 +1100

    Added race.py (1)

--- race.py
+++ race.py
@@ -1,9 +1,18 @@
 from turtle import Turtle, Screen
+import random
 
+def dist():
+    return random.randint(0,10)
+
+def win(turtle):
+    if turtle.xcor() > 230:
+        return True
+    else:
+        return False
 
 screen = Screen()
 screen.setup(width=500, height=500)
-user_pick = screen.textinput(title="Colour", prompt="Turtle colour")
+#user_pick = screen.textinput(title="Colour", prompt="Turtle colour")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 t1 = Turtle(shape="turtle")
@@ -35,6 +44,32 @@ t6 = Turtle(shape="turtle")
 t6.color(colors[5])
 t6.penup()
 t6.goto(x=-230, y=50)
+
+while True:
+   t1.forward(dist())
+   if win(t1):
+       print(t1.pencolor())
+       break
+   t2.forward(dist())
+   if win(t2):
+       print(t2.pencolor())
+       break
+   t3.forward(dist())
+   if win(t3):
+       print(t3.pencolor())
+       break
+   t4.forward(dist())
+   if win(t4):
+       print(t4.pencolor())
+       break
+   t5.forward(dist())
+   if win(t5):
+       print(t5.pencolor())
+       break
+   t6.forward(dist())
+   if win(t6):
+       print(t6.pencolor())
+       break
 
 screen.exitonclick()
 
