00020c3255aa5c09f07e23548c4a2f67657df32a
Manuel Carvalho <burrowsrd@gmail.com>
2021-01-05 14:14:08 +1100

    Added snake.py and snake_game (OOP)2

--- snake.py
+++ snake.py
@@ -8,6 +8,7 @@ class Snake:
         def __init__(self):
             self.segments = []
             self.create_snake()
+            self.head = self.segments[0]
 
         def create_snake(self):
             for postion in STARTING_POSITIONS:
@@ -22,4 +23,16 @@ class Snake:
                 new_x = self.segments[segnum - 1].xcor()
                 new_y = self.segments[segnum - 1].ycor()
                 self.segments[segnum].goto(new_x,new_y)
-            self.segments[0].forward(MOVE_DISTANCE)
+            self.head.forward(MOVE_DISTANCE)
+
+        def up(self):
+            self.head.setheading(90)
+
+        def down(self):
+            self.head.setheading(270)
+
+        def left(self):
+            self.head.setheading(180)
+
+        def right(self):
+            self.head.setheading(0)
