84c9054b2803b34baa1c05bf961f6f799e2b3d41
Manuel Carvalho <burrowsrd@gmail.com>
2021-01-20 13:01:27 +1100

    Added pong score

--- pong_game.py
+++ pong_game.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from pong_board import Scoreboard
 import time
 
 
@@ -14,7 +15,7 @@ r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
 ball = Ball()
 
-
+score = Scoreboard()
 # paddle = Turtle()
 # paddle.shape("square")
 # paddle.color("white")
@@ -32,7 +33,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -41,6 +42,14 @@ while game_is_on:
 
     if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(r_paddle) < 50 and ball.xcor() < -340:
         ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score.r_point()
 
 
 screen.exitonclick()
